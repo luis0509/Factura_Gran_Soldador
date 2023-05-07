227f89e2580e0d51d39b27b958cdc827a0a289d2
luis0509 <122644963+luis0509@users.noreply.github.com>
2023-05-06 20:43:52 -0500

    Update Main.java

--- FACTURA_MDI/origen/VISTA/Main.java
+++ FACTURA_MDI/origen/VISTA/Main.java
@@ -6,13 +6,11 @@ import MODELO.menufac;
 
 public class Main {
     public static void main(String[] args) {
-        //login log_fac = new login();
-        //log_fac.setVisible(true);
-        menufac men = new menufac();
-        men.setVisible(true);
-        //rue
-        //ht/tps://github.com/luis0509/Factura_Gran_Soldador.git
-        //prueba2
+        login log_fac = new login();
+        log_fac.setVisible(true);
+        //menufac men = new menufac();
+        //men.setVisible(true);
+        
     }
     
 }
