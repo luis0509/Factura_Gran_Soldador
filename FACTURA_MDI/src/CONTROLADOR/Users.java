cdc098905b5ea2d7010e39008213887fd2c66ebb
luis0509 <122644963+luis0509@users.noreply.github.com>
2023-05-06 20:48:21 -0500

    Add files via upload

---
+++ FACTURA_MDI/src/CONTROLADOR/Users.java
@@ -0,0 +1,13 @@
+
+package CONTROLADOR;
+
+
+public class Users {
+    
+    public static String usr1="vendedor1";
+    public static String clave1="123";
+    public static String usr2="vendedor2";
+    public static String clave2="321";
+            
+    
+}
