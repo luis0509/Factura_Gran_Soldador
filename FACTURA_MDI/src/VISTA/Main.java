b8254f6c3d65205efeb8b3f069ecab31615ae6c0
Usuario1 <Usuario1@DESKTOP-LAS9AIG>
2023-05-10 21:04:27 -0500

    10/05/23 9:04PM

--- FACTURA_MDI/src/VISTA/Main.java
+++ FACTURA_MDI/src/VISTA/Main.java
@@ -6,11 +6,11 @@ import MODELO.menufac;
 
 public class Main {
     public static void main(String[] args) {
-        //login log_fac = new login();
-        //log_fac.setVisible(true);
-        menufac men = new menufac();
-        men.setVisible(true);
-        //FIN
+        login log_fac = new login();
+        log_fac.setVisible(true);
+        //menufac men = new menufac();
+        //men.setVisible(true);
+        //FIN  
     }
     
 }
