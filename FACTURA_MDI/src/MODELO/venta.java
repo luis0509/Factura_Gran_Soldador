949aaf3faa63246c3c6e9b07b5aaa00f189654ad
emanuel <emanuel@DESKTOP-7F1U9QE>
2023-05-19 23:46:20 -0500

    19/05/23 11:45pm

---
+++ FACTURA_MDI/src/MODELO/venta.java
@@ -0,0 +1,57 @@
+
+package MODELO;
+
+
+public class venta {
+    
+    
+    int id;
+    String descripcion;
+    int cantidad;
+    double precio;
+    double iva;
+
+    public venta() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public double getIva() {
+        return iva;
+    }
+
+    public void setIva(double iva) {
+        this.iva = iva;
+    }
+
+}
